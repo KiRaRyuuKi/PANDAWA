fe29b093dbde99963f59c004aa22316e64483e8a
Muhammad Ilham <M.Ilham.v.28.07.2003@gmail.com>
2025-05-04 15:31:33 +0700

    Fix Issue

--- src/app/map/page.tsx
+++
@@ -1,9 +0,0 @@
-import AppMap from '@/layout/AppMap';
-
-export default function MapPage() {
-    return (
-        <main className="min-h-screen">
-            <AppMap />
-        </main>
-    );
-}
